bc598e80ec0c3a9156043e227a42e6160ae704bd
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-10-07 13:13:26 +0300

    8. Optional Enhancing the App

--- models/product.js
+++ models/product.js
@@ -15,11 +15,15 @@ const getProductsFromFile = (callback) => {
 }
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title
+    this.imageUrl = imageUrl
+    this.description = description
+    this.price = price
   }
 
   save() {
+    // getProductsFromFile принимает коллбэк
     getProductsFromFile((products) => {
       products.push(this)
 
