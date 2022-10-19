cd3770dcde6c223d1e5303f6affababa27c56311
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-10-19 09:47:31 +0300

    Модель User и добавление пользователя в объект запроса

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error')
-// const User = require('./models/user')
+const User = require('./models/user')
 
 const app = express()
 
@@ -24,16 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // static - для всех входящих запросов определям папку со статическими файлами (стили, изображения, шрифты и т.д.)
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req, res, next) => {
-//   User.findById('634beefab97907ee68be5ad6')
-//     .then((user) => {
-//       // На основании данных из БД мы создаем JS-объект user, в котором будут хранится те же данные, что и в БД (имя, фамилия, id)
-//       // Чтобы иметь возможность легко работать с ним в JS
-//       req.user = new User(user.name, user.email, user.cart, user._id)
-//       next()
-//     })
-//     .catch((err) => console.log('Error from app.js app.use(): ', err))
-// })
+app.use((req, res, next) => {
+  User.findById('634ec2cb03f75fdfb8298fc7')
+    .then((user) => {
+      // Объект user вместе с запросом будет прокинут по всем остальным middleware в приложении
+      req.user = user
+      next()
+    })
+    .catch((err) => console.log('Error from app.js app.use(): ', err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -44,6 +43,23 @@ app.use(errorController.get404)
 mongoose
   .connect('mongodb+srv://testuser:testpassword@cluster0.n9aceoe.mongodb.net/shop?retryWrites=true&w=majority')
   .then((result) => {
+    // findOne - найдет первого пользователя в коллекции
+    // Это добавлено для того, чтобы не создавать пользователя при каждом старте
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'Ivan',
+          email: 'ivan@test.com',
+          cart: {
+            items: [],
+          },
+        })
+
+        // save - метод из mongoose
+        user.save()
+      }
+    })
+
     app.listen(3000)
   })
   .catch((err) => console.log('Error from app.js mongoose.connect(): ', err))
--- models/user.js
+++ models/user.js
@@ -1,175 +1,25 @@
-// const mongodb = require('mongodb')
-// const getDb = require('../util/database').getDb
+const mongoose = require('mongoose')
 
-// const ObjectId = mongodb.ObjectId
+const Schema = mongoose.Schema
 
-// class User {
-//   constructor(username, email, cart, id) {
-//     this.name = username
-//     this.email = email
-//     this.cart = cart // {items:[]}
-//     this._id = id
-//   }
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        // productId - id продукта, которое создается автоматически Mongo и обуспечит в дальнейшем связь между документами в коллекциях
+        productId: { type: Schema.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+})
 
-//   save() {
-//     const db = getDb()
-
-//     return db.collection('users').insertOne(this)
-//   }
-
-//   addToCart(product) {
-//     const cartProductIndex = this.cart.items.findIndex((cartProduct) => {
-//       // Чтобы убедиться, что оба id имееют один тип, приводим их к строке
-//       return cartProduct.productId.toString() === product._id.toString()
-//     })
-
-//     let newQuantity = 1
-//     const updatedCartItems = [...this.cart.items]
-
-//     // Если в корзине уже есть товар, то увеличиваем количество на 1
-//     // Если нет, то newQuantity = 1
-//     if (cartProductIndex >= 0) {
-//       newQuantity = this.cart.items[cartProductIndex].quantity + 1
-
-//       updatedCartItems[cartProductIndex].quantity = newQuantity
-//     } else {
-//       updatedCartItems.push({
-//         productId: new ObjectId(product._id),
-//         quantity: newQuantity,
-//       })
-//     }
-
-//     const udpatedCart = {
-//       items: updatedCartItems,
-//     }
-
-//     const db = getDb()
-
-//     return db.collection('users').updateOne(
-//       { _id: new ObjectId(this._id) },
-//       {
-//         // На найденном по ID документе user в БД мы обновляем только одно поле cart
-//         // Т.е. в set передаем объект с полями, которые надо обновить
-//         $set: {
-//           cart: udpatedCart,
-//         },
-//       }
-//     )
-//   }
-
-//   getCart() {
-//     const db = getDb()
-
-//     const productIds = this.cart.items.map((item) => {
-//       return item.productId
-//     })
-
-//     // Получение всех продуктов, чей ID содержится в массиве $in
-//     // Получаем в виде объекта cursor, который трансформируем с помощью toArray()
-//     return db
-//       .collection('products')
-//       .find({
-//         _id: {
-//           $in: productIds,
-//         },
-//       })
-//       .toArray()
-//       .then((products) => {
-//         // Добавляем в каждый продукт количество этого продукта в корзине
-//         return products.map((product) => {
-//           // Возвращаем объект продукта с количеством этого продукта в корзине
-//           return {
-//             ...product,
-//             // Получаем количество продукта в корзине
-//             quantity: this.cart.items.find((item) => {
-//               // Чтобы убедиться, что оба id имееют один тип, приводим их к строке
-//               return item.productId.toString() === product._id.toString()
-//             }).quantity,
-//             //
-//           }
-//         })
-//       })
-//   }
-
-//   deleteItemFromCart(productId) {
-//     const updatedCartItems = this.cart.items.filter((item) => {
-//       // Чтобы убедиться, что оба id имееют один тип, приводим их к строке
-//       return item.productId.toString() !== productId.toString()
-//     })
-
-//     const db = getDb()
-
-//     return db.collection('users').updateOne(
-//       { _id: new ObjectId(this._id) },
-//       {
-//         // На найденном по ID документе user в БД мы обновляем только одно поле cart
-//         // Т.е. в set передаем объект с полями, которые надо обновить
-//         $set: {
-//           cart: { items: updatedCartItems },
-//         },
-//       }
-//     )
-//   }
-
-//   addOrder() {
-//     const db = getDb()
-
-//     // Добавляем доп данные в заказ: данные о user, расширенные данные о товарах (название и пр.)
-//     return (
-//       this.getCart()
-//         .then((products) => {
-//           const order = {
-//             items: products,
-//             user: {
-//               _id: new ObjectId(this._id),
-//               name: this.name,
-//             },
-//           }
-
-//           return db.collection('orders').insertOne(order)
-//         })
-//         // Очищаем корзину после формирования заказа
-//         .then((result) => {
-//           this.cart = {
-//             items: [],
-//           }
-
-//           return db.collection('users').updateOne(
-//             { _id: new ObjectId(this._id) },
-//             {
-//               // На найденном по ID документе user в БД мы обновляем только одно поле cart
-//               // Т.е. в set передаем объект с полями, которые надо обновить
-//               $set: {
-//                 cart: { items: [] },
-//               },
-//             }
-//           )
-//         })
-//     )
-//   }
-
-//   // Получение всех заказов пользователя
-//   getOrders() {
-//     const db = getDb()
-
-//     return db
-//       .collection('orders')
-//       .find({ 'user._id': new ObjectId(this._id) })
-//       .toArray()
-//   }
-
-//   static findById(userId) {
-//     const db = getDb()
-
-//     // next возвращает первый (или последний?) из найденных документов
-//     return db
-//       .collection('users')
-//       .findOne({ _id: new ObjectId(userId) })
-//       .then((user) => {
-//         return user
-//       })
-//       .catch((err) => console.log('Error from User.findById: ', err))
-//   }
-// }
-
-// module.exports = User
+module.exports = mongoose.model('User', userSchema)
